f98c91502cd7ed2e75118d8da27cd2c7a3469dde
eccoMarux <mariaelvira.monserratvidal@yahoo.com>
2022-05-19 01:06:18 -0300

    inicio proyecto

---
+++ src/Censista/Censista.java
@@ -0,0 +1,103 @@
+package Censista;
+public class Censista {
+    private int dni, telefono, area, totalViviendas, cantCensadas;
+    private String provincia, ciudad;
+
+    // Contructores
+    public Censista(int dni, int telefono, int area, int totalViviendas, int cantCensadas, String provincia,
+            String ciudad) {
+        this.dni = dni;
+        this.telefono = telefono;
+        this.area = area;
+        this.totalViviendas = totalViviendas;
+        this.cantCensadas = cantCensadas;
+        this.provincia = provincia;
+        this.ciudad = ciudad;
+    }
+
+    public Censista(int dni) {
+        this.dni = dni;
+        this.telefono = 0;
+        this.area = -1;
+        this.totalViviendas = -1;
+        this.cantCensadas = -1;
+        this.provincia = " ";
+        this.ciudad = " ";
+    }
+
+    // Observadoras
+    public int getDni() {
+        return this.dni;
+    }
+
+    public int getTelefono() {
+        return this.telefono;
+    }
+
+    public int getArea() {
+        return this.area;
+    }
+
+    public int getTotalViviendas() {
+        return this.totalViviendas;
+    }
+
+    public int getCantCensadas() {
+        return this.cantCensadas;
+    }
+
+    public String getProvincia() {
+        return this.provincia;
+    }
+
+    public String getCiudad() {
+        return this.ciudad;
+    }
+
+    // Modificadoras
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    public void setTotalViviendas(int totalViviendas) {
+        this.totalViviendas = totalViviendas;
+    }
+
+    public void setCantCensadas(int cantCensadas) {
+        this.cantCensadas = cantCensadas;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    // Propias del tipo
+    public boolean equals(Censista objeto) {
+        boolean iguales = false;
+        if (this.dni == objeto.getDni()) {
+            iguales = true;
+        }
+        return iguales;
+    }
+
+    public String toString() {
+        String cadena;
+        cadena = "Dni: " + this.dni + " \nTelefono: " + this.telefono + " \nArea: " + this.area +
+                "\nTotal Viviendas: " + this.totalViviendas + "\nCantidad Censadas: " + this.cantCensadas +
+                "\nProvincia: " + this.provincia + "\nCiudad: " + this.ciudad;
+        return cadena;
+    }
+
+    public void censo(int cantViviendas) {
+        this.cantCensadas = this.cantCensadas + cantViviendas;
+    }
+
+}
---
+++ src/Censista/testCensita.java
@@ -0,0 +1,38 @@
+package Censista;
+
+public class testCensita {
+    public static void main(String[] args) {
+        Censista pepe, ana, pablo, laura;
+        pepe = new Censista(123, 321, 12, 20, 17, "Neuquen", "Neuquen");
+        ana = new Censista(233, 454, 13, 20, 10, "Neuquen", "Neuquen");
+        pablo = new Censista(789, 654, 14, 20, 14, "Neuquen", "Neuquen");
+        laura = new Censista(456, 564, 15, 20, 18, "Neuquen", "Neuquen");
+        Censista[] personas = { pepe, ana, pablo, laura };
+
+        // Estructura improvisada para testear el modulo.
+        for (int i = 0; i < 4; i++) {
+            if (censoMasSetenta(personas)[i] != null) {
+                System.out.println(censoMasSetenta(personas)[i].toString());
+                System.out.println("-----------------------------");
+            } else {
+                System.out.println("null");
+                System.out.println("-----------------------------");
+            }
+        }
+    }
+
+    public static Censista[] censoMasSetenta(Censista[] arreglo) {
+        Censista[] arregloPersonas = new Censista[arreglo.length];
+        int i, j = 0;
+        double porcentaje;
+        for (i = 0; i < arreglo.length; i++) {
+            porcentaje = (arreglo[i].getTotalViviendas() * 70) / 100;
+            if (arreglo[i].getCantCensadas() > porcentaje) {
+                arregloPersonas[j] = arreglo[i];
+                j++;
+            }
+        }
+        return arregloPersonas;
+    }
+
+}
